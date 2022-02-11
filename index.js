1a6ce36ed25cc8d4ee0b791a002a9eb0f29dc716
sudotrololo <sudotrololo@gmail.com>
2022-02-11 03:52:06 +0300

    new_fix

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 //clear document
 //master
 //enother commit
+function f (){
+  console.log('azaza')
+  console.log('First')
+  console.log('second')
+}
 const func = () => {
   console.log("FIRST FUNCTION");
 };
+//new comment
